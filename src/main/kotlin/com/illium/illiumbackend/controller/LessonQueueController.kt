5d9910c0b166585a0efbbd3af05bb46ea229304a
Ethan <ethanherrera@berkeley.edu>
2025-01-13 19:16:59 -0800

    basic crud for queues working

---
+++ src/main/kotlin/com/illium/illiumbackend/controller/LessonQueueController.kt
@@ -0,0 +1,58 @@
+package com.illium.illiumbackend.controller
+
+// create a springboot controller for the event service
+import com.illium.illiumbackend.model.*
+import com.illium.illiumbackend.service.EventService
+import com.illium.illiumbackend.service.LessonQueueService
+import com.illium.illiumbackend.service.TechniqueService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/lesson-queues")
+class LessonQueueController(
+    private val lessonQueueService: LessonQueueService
+) {
+
+    @GetMapping
+    fun getAllLessonQueues() : ResponseEntity<List<LessonQueueResponse>> {
+        return try {
+            val lessonQueues = lessonQueueService.getAllLessonQueues()
+            ResponseEntity(lessonQueues, HttpStatus.OK)
+        } catch (e: Exception) {
+            ResponseEntity(HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @PostMapping
+    fun createLessonQueue(@RequestBody lessonQueueRequest: LessonQueueRequest) : ResponseEntity<LessonQueueResponse> {
+        return try {
+            val event = lessonQueueService.createLessonQueue(lessonQueueRequest)
+            ResponseEntity(event, HttpStatus.CREATED)
+        } catch (e: Exception) {
+            ResponseEntity(HttpStatus.BAD_REQUEST)
+        }
+
+    }
+
+    @PutMapping("/{id}")
+    fun editLessonQueue(@RequestBody lessonQueueRequest: LessonQueueRequest, @PathVariable id: Long) : ResponseEntity<LessonQueueResponse> {
+        return try {
+            val event = lessonQueueService.editLessonQueue(lessonQueueRequest, id)
+            ResponseEntity(event, HttpStatus.OK)
+        } catch (e: Exception) {
+            ResponseEntity(HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteLessonQueue(@PathVariable id: Long) : ResponseEntity<Unit> {
+        return try {
+            lessonQueueService.deleteLessonQueue(id)
+            ResponseEntity(Unit, HttpStatus.OK)
+        } catch (e: Exception) {
+            ResponseEntity(HttpStatus.BAD_REQUEST)
+        }
+    }
+}
--- src/main/kotlin/com/illium/illiumbackend/service/LessonQueueService.kt
+++ src/main/kotlin/com/illium/illiumbackend/service/LessonQueueService.kt
@@ -1,6 +1,6 @@
 package com.illium.illiumbackend.service
 
-import com.illium.illiumbackend.model.LessonQueue
+import com.illium.illiumbackend.model.*
 import com.illium.illiumbackend.repository.LessonQueueRepository
 import org.springframework.stereotype.Service
 import java.lang.Exception
@@ -14,5 +14,29 @@ class LessonQueueService(
     }
 
     fun findLessonQueueByLevel(level: Int) : List<LessonQueue> = lessonQueueRepository.findByLevel(level)
+
+    fun createLessonQueue(lessonQueueRequest: LessonQueueRequest) : LessonQueueResponse {
+        val lessonQueue = lessonQueueRequest.toEntity()
+        lessonQueueRepository.save(lessonQueue)
+        return lessonQueue.toResponse()
+    }
+
+    fun editLessonQueue(lessonQueueRequest: LessonQueueRequest, id: Long) : LessonQueueResponse {
+        val lessonQueue = lessonQueueRepository.findById(id).orElseThrow {
+            Exception("Technique with id $id not found")
+        }
+        lessonQueue.name = lessonQueueRequest.name
+        lessonQueue.level = lessonQueueRequest.level
+        lessonQueueRepository.save(lessonQueue)
+        return lessonQueue.toResponse()
+    }
+
+    fun deleteLessonQueue(id: Long) {
+        lessonQueueRepository.deleteById(id)
+    }
+
+    fun getAllLessonQueues() : List<LessonQueueResponse> {
+        return lessonQueueRepository.findAll().map { it.toResponse() }
+    }
 
 }
